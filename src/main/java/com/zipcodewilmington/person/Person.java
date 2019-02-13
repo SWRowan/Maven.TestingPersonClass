d140dc601a66faae7fbb6b4c58406325b13b8747
Sean Rowan <seanrowan@zipcoders-MacBook-Pro.local>
2019-02-13 17:20:40 -0500

    Completed person test lab

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -1,35 +1,93 @@
 package com.zipcodewilmington.person;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
 /**
  * Created by leon on 2/12/18.
  */
 public class Person {
-    private String name;
-    private int age;
+    private String name = "";
+    private int age = Integer.MAX_VALUE;
+    private Boolean isFemale = true;
+    private Boolean hasFacialHair = false;
+    private Double weight = 200.0;
+    private String eyeColor = "Brown";
+    private Boolean isAwesome = true;
+
+
+
 
     public Person() {
+
+
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+        this.name = name;
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+       this.age = age;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+        return age;
+    }
+
+    public Boolean getFemale() {
+        return isFemale;
+    }
+
+    public void setFemale(Boolean female) {
+        isFemale = female;
+    }
+
+    public Boolean getHasFacialHair() {
+        return hasFacialHair;
+    }
+
+    public void setHasFacialHair(Boolean hasFacialHair) {
+        this.hasFacialHair = hasFacialHair;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public String getEyeColor() {
+        return eyeColor;
+    }
+
+    public void setEyeColor(String eyeColor) {
+        this.eyeColor = eyeColor;
+    }
+
+    public Boolean getAwesome() {
+        return isAwesome;
+    }
+
+    public void setAwesome(Boolean awesome) {
+        isAwesome = awesome;
     }
 }
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -12,6 +12,7 @@ public class TestPerson {
         // Given
         String expectedName = "";
         Integer expectedAge = Integer.MAX_VALUE;
+        Double expectedWeight = 200.0;
 
         // When
         Person person = new Person();
@@ -19,9 +20,16 @@ public class TestPerson {
         // Then
         String actualName = person.getName();
         Integer actualAge = person.getAge();
+        Boolean actualSex = person.getFemale();
+        Boolean actualFacialHair = person.getHasFacialHair();
+        Double actualWeight = person.getWeight();
 
         Assert.assertEquals(expectedName, actualName);
         Assert.assertEquals(expectedAge, actualAge);
+        Assert.assertTrue(actualSex);
+        Assert.assertFalse(actualFacialHair);
+        Assert.assertEquals(expectedWeight, actualWeight);
+
     }
 
     @Test
@@ -94,5 +102,128 @@ public class TestPerson {
         // Then
         Integer actual = person.getAge();
         Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getIsFemaleTest(){
+        //Given
+        Person person = new Person();
+
+
+        //When
+        Boolean actual = person.getFemale();
+
+        //Then
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void setIsFemaleTest(){
+        //Given
+        Person person = new Person();
+        person.setFemale(false);
+
+        //When
+        Boolean actual = person.getFemale();
+
+        //Then
+        Assert.assertFalse(actual);
+    }
+
+    @Test
+    public void getFacialHairTest(){
+        //Given
+        Person person = new Person();
+
+        //When
+        Boolean actual = person.getHasFacialHair();
+
+        //Then
+        Assert.assertFalse(actual);
+    }
+
+    @Test
+    public void setFacialHairTest(){
+        //Given
+        Person person = new Person();
+        person.setHasFacialHair(true);
+
+        //When
+        Boolean actual = person.getHasFacialHair();
+
+        //Then
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void getWeightTest(){
+        //Given
+        Person person = new Person();
+        Double expected = 200.0;
+
+        //When
+        Double actual = person.getWeight();
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setWeightTest(){
+        //Given
+        Person person = new Person();
+        person.setWeight(255.8);
+        Double expected = 255.8;
+
+        //When
+        Double actual = person.getWeight();
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getEyeColorTest(){
+        //Given
+        Person person = new Person();
+        String expected = "Brown";
+
+        //When
+        String actual = person.getEyeColor();
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setEyeColorTest(){
+        //Given
+        Person person = new Person();
+        person.setEyeColor("Blue");
+        String expected = "Blue";
+        //When
+        String actual = person.getEyeColor();
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getIsAwesomeTest(){
+        //Given
+        Person person = new Person();
+        //When
+        Boolean actual = person.getAwesome();
+        //Then
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void setIsAwesomeTest(){
+        //Given
+        Person person = new Person();
+        person.setAwesome(false);
+        //When
+        Boolean actual = person.getAwesome();
+        //Then
+        Assert.assertFalse(actual);
     }
 }
